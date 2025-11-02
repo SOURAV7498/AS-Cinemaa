ddca3a1df1044d17b998534f5846640887cb3ed5
SOURAV7498 <gouravbolange@gmail.com>
2025-11-02 23:03:07 +0530

    Update caption links and provider names in Script.py

--- Script.py
+++ Script.py
@@ -211,7 +211,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/as_cinemaamovies">{file_name}</a></b>\n\n<b>⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ: <a href="https://t.me/as_cinemaamovies">[ ᴀꜱ ᴄɪɴᴇᴍᴀᴀ ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/Movie_Provider_group">{file_name}</a></b>\n\n<b>⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ: <a href="https://t.me/Movie_Provider_group">[ Movɪeʀoll ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
