7931a86f35c781e856133b0f29d4a8101a12fa9f
Nonuppt <159997847+Nonuppt@users.noreply.github.com>
2025-11-05 22:30:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -114,7 +114,7 @@ SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "shortxlinks.com") # Shor
 SHORTENER_API3 = environ.get("SHORTENER_API3", "")  
 SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "") # Shortener website for third website
 
-TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "1200")) # Time gap for two-step verification in seconds (default: 20 minutes)
+TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "54000")) # Time gap for two-step verification in seconds (default: 20 minutes)
 THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))    
 
 # ============================
