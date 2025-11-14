8e195f4ab44b0f6f58b1f838ef0ee36db25c599b
Nonuppt <159997847+Nonuppt@users.noreply.github.com>
2025-11-14 12:30:53 +0530

    Update new_movies_notify.py

--- new_movies_notify.py
+++ new_movies_notify.py
@@ -3,7 +3,7 @@ from database.ia_filterdb import Media
 from info import LOG_CHANNEL, ADMINS
 
 # ⚠️ Set your movie upload channel ID here
-SOURCE_MOVIE_CHANNEL = -1002837138676   # <-- Change this
+SOURCE_MOVIE_CHANNEL = -6277856017   # <-- Change this
 
 
 # Helper: Safe send message
