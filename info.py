167898f0da9a9fd1f2ddac70144cb3ad2eba68b9
Movieroll bot's <gouravbolange@gmail.com>
2025-11-28 14:08:06 +0530

    Change default BOT_TOKEN to empty string
    Updated default BOT_TOKEN to an empty string for security.

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'dreamxbotz_search')   # Session name for the bot
 API_ID = int(environ.get('API_ID', '21006440')) # API ID from my.telegram.org
 API_HASH = environ.get('API_HASH', 'f3b3fae13d20ab4b5f34ac3475749c20')  # API Hash from my.telegram.org
-BOT_TOKEN = environ.get('BOT_TOKEN', "8503469545:AAHL-puFbC17FEVrs5JlG5RW0zK-_GzBOYw")    # Bot token from @BotFather
+BOT_TOKEN = environ.get('BOT_TOKEN', "")    # Bot token from @BotFather
 
 # ============================
 # Bot Settings Configuration
