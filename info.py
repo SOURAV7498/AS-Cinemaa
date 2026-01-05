46e7bb54742e650d6d2199912bb9018b154a6c53
Movieroll bot's <gouravbolange@gmail.com>
2026-01-05 15:21:33 +0530

    Set default values for additional shortener APIs

--- info.py
+++ info.py
@@ -108,11 +108,11 @@ TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/How_to_get_AS_Links/6")   #
 SHORTENER_API = environ.get("SHORTENER_API", "c7d26524bec5a47ef34454ed1348511f4124bda6") # Shortener API key
 SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "http://shortxlinks.com") # Shortener website
 
-SHORTENER_API2 = environ.get("SHORTENER_API2", "")  # Shortener API key for second website
-SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "") # Shortener website for second website
+SHORTENER_API2 = environ.get("SHORTENER_API2", "c7d26524bec5a47ef34454ed1348511f4124bda6")  # Shortener API key for second website
+SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "http://shortxlinks.com") # Shortener website for second website
 
-SHORTENER_API3 = environ.get("SHORTENER_API3", "")  
-SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "") # Shortener website for third website
+SHORTENER_API3 = environ.get("SHORTENER_API3", "c7d26524bec5a47ef34454ed1348511f4124bda6")  
+SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "http://shortxlinks.com") # Shortener website for third website
 
 TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "1200")) # Time gap for two-step verification in seconds (default: 20 minutes)
 THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))    
